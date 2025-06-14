9afccfa39ad851c4cb4bf1b6069dcc3db2a2e40d
prashant <prashantyadav852@gmail.com>
2025-06-14 23:54:56 +0530

    Moved User Card to Home Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
-import UserCard from "./components/UserCard";
-import PrashantImg from "./assets/image/Prashant.jpg";
-import salman from "./assets/image/salman.jpeg";
-import virat from "./assets/image/RCB_Virat.jpg";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -38,27 +35,12 @@ const router = createBrowserRouter([
     ),
   },
 ]);
-const users = [
-  { name: "Prashant", image: PrashantImg, address: "Lucknow" },
-  { name: "Salman", image: salman, address: "Mumbai" },
-  { name: "Virat", image: virat, address: "Bangalore" },
-];
 
 function App() {
   return (
     <>
       <div>
         <RouterProvider router={router} />
-      </div>
-      <div className="container">
-        {users.map((user, index) => (
-          <UserCard
-            id={index}
-            name={user.name}
-            image={user.image}
-            address={user.address}
-          />
-        ))}
       </div>
     </>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import PrashantImg from "../assets/image/Prashant.jpg";
+import salman from "../assets/image/salman.jpeg";
+import virat from "../assets/image/RCB_Virat.jpg";
+import UserCard from "./UserCard";
+
+const users = [
+  { name: "Prashant", image: PrashantImg, address: "Lucknow" },
+  { name: "Salman", image: salman, address: "Mumbai" },
+  { name: "Virat", image: virat, address: "Bangalore" },
+];
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welocme to Home Page</h1>
-    </div>
+    <>
+      <div>
+        <h1>Welocme to Home Page</h1>
+      </div>
+      <div className="container">
+        {users.map((user, index) => (
+          <UserCard
+            id={index}
+            name={user.name}
+            image={user.image}
+            address={user.address}
+          />
+        ))}
+      </div>
+    </>
   );
 }
